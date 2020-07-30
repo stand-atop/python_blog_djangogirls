aee31963a85512a69e40e8afce21bcc04627930c
stand-atop <stand.atop@gmail.com>
2020-07-30 17:28:29 +0900

    Modifyed templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # 요청(request)을 받아 render메서드를 호출해 blog/post_list.html 템플릿을 보여주는 함수
 def post_list(request):
-        return render(request, 'blog/post_list.html', {})
-
-        
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
